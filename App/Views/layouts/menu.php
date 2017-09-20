42e77525d52a32c7360397256cd308d70397961d
Handerson Silva <handersonsylva@gmail.com>
2017-09-20 20:58:08 -0300

    Consertando Erro nas rotas

--- App/Views/layouts/menu.php
+++ App/Views/layouts/menu.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="../">Vazamentos</a>
+        <a class="navbar-brand" href="http://<?php echo APP_HOST; ?> ">Vazamentos</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
     <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="../">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="http://<?php echo APP_HOST; ?>">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="usuario/Login">Logar</a>
+                <a class="nav-link" href="http://<?php echo APP_HOST; ?>usuario/Login">Logar</a>
             </li>
         </ul>
     
