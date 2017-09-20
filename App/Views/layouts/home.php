41bf906401c337181705a243c13a1d06b9b3b16e
Handerson Silva <handersonsylva@gmail.com>
2017-09-20 20:31:47 -0300

    Pegando a localização do usuario

--- App/Views/layouts/home.php
+++ App/Views/layouts/home.php
@@ -13,14 +13,27 @@
                 };
                 //setando o mapa dentro da div mapa
                 map = new google.maps.Map(document.getElementById("mapa"),param);
+
+                //pegando a localização do usuario
+                if(navigator.geolocation){
+                        navigator.geolocation.getCurrentPosition(function(position){
+                                localizacaoUser = new google.maps.LatLng(
+                                        position.coords.latitude,
+                                        position.coords.longitude
+                                        );
+                                //setando no mapa
+                                map.setCenter(localizacaoUser);
+                        })
+                }
              }
 
+             //Chamando a função inicial
             google.maps.event.addDomListener(window,'load',init);
       </script>
        <!--Estilo da div mapa-->
        <style>
              #mapa{
-                 width:1110px;
+                 width:1040px;
                  height:500px;
                  border:solid black;
                  }
@@ -30,10 +43,16 @@
      <br/>
         <div class="container" style="margin-left: 5px">
             <div class="row">
-                    <div class="col-md-2">
-                        <a href="#">Cadastra nava reclamção</a>
+                    <div class="col-md-3">
+                        <a href="#">Cadastra nova reclamção</a>
+                        <form method="post" action="#">
+                                Descricão:<input type="text" name="descricaoV" required>
+                                <!--campos ocultos-->
+                                <input type="hidden" name="latP" required>
+                                <input type="hidden" name="logP" required>
+                        </form>
                     </div>
-                    <div class="col-md-10">
+                    <div class="col-md-9" >
                             <div id="mapa">
                             </div>
                     </div>
