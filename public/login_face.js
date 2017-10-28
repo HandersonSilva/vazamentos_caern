445c86034f187c0287449fc92f3a065bc913e033
Handerson Silva <handersonsylva@gmail.com>
2017-10-27 21:39:41 -0300

    Testando no servidor Facebook 1

--- public/login_face.js
+++ public/login_face.js
@@ -103,7 +103,7 @@ FB.Event.subscribe('auth.login', function () {
             data: {login:loginFace},
             success:function(data){
         
-               
+                alert("Conectado ao face"+data);
                 
             },
             error: function (result) {
